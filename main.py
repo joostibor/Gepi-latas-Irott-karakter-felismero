b44b49ee27e74055001cb74fd3d8cf8f27961330
joostibor <joostibor98@gmail.com>
2020-11-09 14:48:07 +0100

    Add digital test and make some code

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+print(tf.__version__)
 
 #Dataset betöltése és trainingje
-mnistnums = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnistnums.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 #Adatok normalizálása
 x_train = tf.keras.utils.normalize(x_train, axis=1)
@@ -14,7 +14,7 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 #Modell készítése, rétegek hozzáadása
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28,28))) #bementi réteg
+model.add(tf.keras.layers.Flatten(input_shape=(28, 28))) #bementi réteg
 model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
 model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax)) #kimeneti réteg
@@ -23,3 +23,10 @@ model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax)) #kimeneti r
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=5)
 model.save('nums.mod')
+
+#Képek beolvasása a teszthez
+for x in range(0, 10):
+    readimg = cv2.imread(f'TestNumbers/{x}.png')[:,:,0]
+    readimg = np.array([readimg])
+    prediction = model.predict(readimg)
+    print(f'The prediction of the sotfware is: {np.argmax(prediction)}')
