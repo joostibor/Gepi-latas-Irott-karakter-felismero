e98d59e9cbd0f673e067c7bc6f556cbb95fd5e6d
joostibor <joostibor98@gmail.com>
2020-11-09 17:00:46 +0100

    little fiy and add tests

--- main.py
+++ main.py
@@ -2,31 +2,30 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
-print(tf.__version__)
+import keras
 
 #Dataset betöltése és trainingje
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
 #Adatok normalizálása
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+x_train = keras.utils.normalize(x_train, axis=1)
+x_test = keras.utils.normalize(x_test, axis=1)
 
 #Modell készítése, rétegek hozzáadása
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28))) #bementi réteg
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax)) #kimeneti réteg
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=(28, 28))) #bementi réteg
+model.add(keras.layers.Dense(units=64, activation='relu'))
+model.add(keras.layers.Dense(units=64, activation='relu'))
+model.add(keras.layers.Dense(units=10, activation='softmax')) #kimeneti réteg
 
 #Modell lefordítása, tanítása és mentése
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=5)
-model.save('nums.mod')
+model.evaluate(x_test, y_test)
 
 #Képek beolvasása a teszthez
-for x in range(0, 10):
+for x in range(0, 20):
     readimg = cv2.imread(f'TestNumbers/{x}.png')[:,:,0]
-    readimg = np.array([readimg])
+    readimg = np.invert(np.array([readimg]))
     prediction = model.predict(readimg)
     print(f'The prediction of the sotfware is: {np.argmax(prediction)}')
