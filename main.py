7d8a51a4ac7552489de10b4644b56dccb0af5f9f
joostibor <joostibor98@gmail.com>
2020-11-17 16:20:13 +0100

    Add more test and some modify

--- main.py
+++ main.py
@@ -1,31 +1,52 @@
 #Szükséges csomagok importálása
+#%%
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-import keras
+import tensorflow as tf
 
 #Dataset betöltése és trainingje
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 #Adatok normalizálása
-x_train = keras.utils.normalize(x_train, axis=1)
-x_test = keras.utils.normalize(x_test, axis=1)
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 #Modell készítése, rétegek hozzáadása
-model = keras.models.Sequential()
-model.add(keras.layers.Flatten(input_shape=(28, 28))) #bementi réteg
-model.add(keras.layers.Dense(units=64, activation='relu'))
-model.add(keras.layers.Dense(units=64, activation='relu'))
-model.add(keras.layers.Dense(units=10, activation='softmax')) #kimeneti réteg
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape = (28,28))) #bementi réteg
+model.add(tf.keras.layers.Dense(units=128, activation= tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=128, activation= tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=10, activation= tf.nn.softmax)) #kimeneti réteg
 
 #Modell lefordítása, tanítása és mentése
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=5)
-model.evaluate(x_test, y_test)
+model.fit(x_train, y_train, batch_size = 32, epochs=5)
+loss, accuracy = model.evaluate(x_test, y_test)
+
+#Modell mentése
+model.save('digits.mod')
 
 #Képek beolvasása a teszthez
-for x in range(0, 20):
+for x in range(0, 10):
+    readimg = cv2.imread(f'TestNumbers/{x}_big.png')[:,:,0]
+    readimg = np.invert(np.array([readimg]))
+    prediction = model.predict(readimg)
+    print(f'Number: {x}, The prediction of the sotfware is: {np.argmax(prediction)}')
+    plt.imshow(readimg[0], cmap=plt.cm.binary)
+    plt.show()
+
+
+for x in range(0, 10):
     readimg = cv2.imread(f'TestNumbers/{x}.png')[:,:,0]
     readimg = np.invert(np.array([readimg]))
     prediction = model.predict(readimg)
-    print(f'The prediction of the sotfware is: {np.argmax(prediction)}')
+    print(f'Number: {x}, The prediction of the sotfware is: {np.argmax(prediction)}')
+
+for x in range(0, 10):
+    readimg = cv2.imread(f'TestNumbers/1{x}.png')[:,:,0]
+    readimg = np.invert(np.array([readimg]))
+    prediction = model.predict(readimg)
+    print(f'Number: {x}, The prediction of the sotfware is: {np.argmax(prediction)}')
+# %%
